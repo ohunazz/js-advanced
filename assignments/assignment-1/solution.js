842ce30ad51a258729cd110767e9f7a8fc966599
ohunazz <ohunazz@gmail.com>
2023-07-24 15:38:51 -0400

    Finish Assignment

--- assignments/assignment-1/solution.js
+++ assignments/assignment-1/solution.js
@@ -140,3 +140,88 @@ Number.prototype.isOdd = function () {
 
 console.log((5).isOdd());
 console.log((6).isOdd());
+
+/* Array.prototype
+
+findMax(): This method returns the maximum value in an array of numbers.
+Example: [1, 2, 3].findMax() should return 3.
+Example: [-1, -2, -3].findMax() should return -1. */
+
+Array.prototype.findMax = function () {
+    let max = this[0];
+    for (let i = 1; i < this.length; i++) {
+        if (this[i] > max) {
+            max = this[i];
+        }
+    }
+    return max;
+};
+
+console.log([1, 2, 3].findMax());
+console.log([-1, -2, -3].findMax());
+
+/* 2. findMin(): This method returns the minimum value in an array of numbers.
+Example: [1, 2, 3].findMin() should return 1.
+Example: [-1, -2, -3].findMin() should return -3. */
+
+Array.prototype.findMin = function () {
+    let min = this[0];
+    for (let i = 1; i < this.length; i++) {
+        if (this[i] < min) {
+            min = this[i];
+        }
+    }
+    return min;
+};
+
+console.log([1, 2, 3].findMin());
+console.log([-1, -2, -3].findMin());
+
+/* 3. average(): This method returns the average of all the numbers in the array.
+Example: [1, 2, 3, 4].average() should return 2.5.
+Example: [5, 10, 15, 20].average() should return 12.5. */
+
+Array.prototype.average = function () {
+    let sum = 0;
+    for (let i = 0; i < this.length; i++) {
+        sum += this[i];
+    }
+    return sum / this.length;
+};
+
+console.log([1, 2, 3, 4].average());
+console.log([5, 10, 15, 20].average());
+
+/* 4. removeDuplicates(): This method returns a new array with duplicates removed.
+Example: [1, 2, 2, 3].removeDuplicates() should return [1, 2, 3].
+Example: ["a", "b", "b", "c"].removeDuplicates() should return ["a", "b", "c"]. */
+
+Array.prototype.removeDuplicates = function () {
+    const result = [];
+    for (let i = 0; i < this.length; i++) {
+        if (!result.includes(this[i])) {
+            result.push(this[i]);
+        }
+    }
+    return result;
+};
+
+console.log([1, 2, 2, 3].removeDuplicates());
+console.log(["a", "b", "b", "c"].removeDuplicates());
+
+/* 5. shuffle(): This method returns a new array with the order of the elements randomized.
+Example: [1, 2, 3].shuffle() could return [3, 1, 2].
+Example: ["a", "b", "c"].shuffle() could return ["c", "a", "b"].
+ */
+
+Array.prototype.shuffle = function () {
+    const array = this.slice();
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+};
+
+console.log([1, 2, 3].shuffle());
+console.log(["a", "b", "c"].shuffle());
