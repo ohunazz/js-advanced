3f1bca8aede9b0cda920aa1057bfcfae4efbec44
ohunazz <ohunazz@gmail.com>
2023-07-24 14:29:56 -0400

    Finish Number Prototype

--- assignments/assignment-1/solution.js
+++ assignments/assignment-1/solution.js
@@ -63,3 +63,80 @@ String.prototype.isPalindrome = function () {
 
 console.log("racecar".isPalindrome());
 console.log("Hello".isPalindrome());
+
+/*  Number.prototype
+1. isPrime(): This method checks if the number is a prime number.
+Example: (7).isPrime() should return true.
+Example: (4).isPrime() should return false. */
+
+Number.prototype.isPrime = function () {
+    if (this <= 1) return false;
+
+    for (let i = 2; i < this; i++) {
+        if (this % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+console.log((5).isPrime());
+console.log((10).isPrime());
+
+/* 2. toFactorial(): This method returns the factorial of the number.
+Example: (5).toFactorial() should return 120.
+Example: (3).toFactorial() should return 6. */
+
+Number.prototype.toFactorial = function () {
+    let factorial = 1;
+    for (let i = 2; i <= this; i++) {
+        factorial *= i;
+    }
+
+    return factorial;
+};
+
+console.log((5).toFactorial());
+console.log((3).toFactorial());
+
+/* 3. findFactors(): This method returns an array of all factors of the number.
+Example: (10).findFactors() should return [1, 2, 5, 10].
+Example: (16).findFactors() should return [1, 2, 4, 8, 16].
+ */
+
+Number.prototype.findFactors = function () {
+    const factors = [];
+    for (let i = 1; i <= this; i++) {
+        if (this % i === 0) {
+            factors.push(i);
+        }
+    }
+    return factors;
+};
+
+console.log((10).findFactors()); // Should return [1, 2, 5, 10]
+console.log((16).findFactors()); // Should return [1, 2, 4, 8, 16]
+
+/* 4. isPerfectSquare(): This method checks if the number is a perfect square.
+Example: (9).isPerfectSquare() should return true.
+Example: (8).isPerfectSquare() should return false. */
+
+Number.prototype.isPerfectSquare = function () {
+    const sqrtValue = Math.sqrt(this);
+    return Number.isInteger(sqrtValue);
+};
+
+console.log((9).isPerfectSquare());
+console.log((8).isPerfectSquare());
+
+/* 5. isOdd(): This method checks if a number is odd.
+Example: (5).isOdd() should return true.
+Example: (6).isOdd() should return false. */
+
+Number.prototype.isOdd = function () {
+    return this % 2 !== 0;
+};
+
+console.log((5).isOdd());
+console.log((6).isOdd());
